43363a831352a3e5987ee33d227664738cf8ac69
jomana <69699199+jomanaehabb@users.noreply.github.com>
2023-10-07 01:17:32 +0300

    Update

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,18 @@
 #!/usr/bin/env ruby
-Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
-List = [Format[0].compact, Format[1].compact, Format[2].compact]
-puts List.join(',')
+log = ARGV[0]
+
+pattern = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
+
+matches = log.scan(pattern)
+
+if matches.any?
+    matches.each do |match|
+      from = match[0]
+      to = match[1]
+      flags = match[2]
+      
+      puts "#{from},#{to},#{flags}"
+    end
+else
+    puts "Pattern not found in the input log."
+end
